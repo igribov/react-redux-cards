728f8b9f03a1eaffdecd74adeb8b4d68bf850d25
Ilya Gribov <igribov@condenast.ru>
2017-10-27 13:08:01 +0300

    fix migration

--- app/DoctrineMigrations/Version20171009082831.php
+++ app/DoctrineMigrations/Version20171009082831.php
@@ -16,7 +16,7 @@ class Version20171009082831 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->abortIf(!in_array($this->connection->getDatabasePlatform()->getName(), ['mysql']), 'Migration can only be executed safely on \'mysql\' or \'sqlite\'.');
-        $this->addSql("CREATE TABLE card id INTEGER NOT NULL AUTO_INCREMENT, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, status VARCHAR(16) NOT NULL, PRIMARY KEY(id)");
+        $this->addSql("CREATE TABLE card (id INTEGER NOT NULL AUTO_INCREMENT, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, status VARCHAR(16) NOT NULL, PRIMARY KEY(id))");
         $this->addSql("INSERT INTO card (id, title, description, status) VALUES (1, 'My new card', 'My new card desc', 'todo')");
     }
 
