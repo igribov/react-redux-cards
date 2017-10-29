b959b813433baed0a3b09180ec6f4043172def0c
Gribov Ilya <imilyagribov@gmail.com>
2017-10-29 11:15:11 +0300

    final versions

--- bin/fronted_dev_server.sh
+++ bin/fronted_dev_server.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 YARN_CMD=$(which yarn)
 
-cd $DIR/../src/FrontBundle/FrontSrc
+cd $DIR/../frontend
 printf "Start Webpack dev server: \n"
 $YARN_CMD start
--- bin/fronted_rebuild.sh
+++ bin/fronted_rebuild.sh
@@ -3,13 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 YARN_CMD=$(which yarn)
 
-cd $DIR/../src/FrontBundle/FrontSrc
+cd $DIR/../frontend
 printf "Start Frontend build: \n"
 $YARN_CMD install
 
 printf "Start Webpack build: \n"
-$YARN_CMD build-resources
-
-cd $DIR/../
-printf "Start Assets install: \n"
-$DIR/console assets:install
+$YARN_CMD build
